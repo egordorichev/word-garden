56d1015cdaadabf92037fb0c585c235a74752dc9
Egor Dorichev <egordorichev@gmail.com>
2020-10-03 13:13:18 +0300

    Camera clipping

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+const SCALE = 4
+
 var client = new Colyseus.Client('ws://localhost:2567');
 var draw = () => {};
 var players = new Map();
@@ -89,8 +91,15 @@ function update() {
 }
 
 function keyPressed() {
-	if (keyCode == 32 && room && !room.inputBlocked) {
-		setModalEnabled(true);
+	if (room && !room.inputBlocked) {
+		// t or /
+		if (keyCode == 84 || keyCode == 191) {
+			setTimeout(() => {
+				input.focus();
+			}, 0.01);
+		} else if (keyCode == 32) {
+			setModalEnabled(true);
+		}
 	}
 }
 
@@ -101,7 +110,7 @@ function setupDraw() {
 		translate(width * 0.5, height * 0.5)
 
 		background(0);
-		scale(4);
+		scale(SCALE);
 
 		if (localPlayer) {
 			var dx = localPlayer.x - cx;
@@ -121,17 +130,51 @@ function setupDraw() {
 			translate(-cx - 4, -cy - 4);
 		}
 
+		stroke(100, 100, 100, 25);	
+		strokeWeight(1);
+
+		const doubleScale = 2 * SCALE;
+
+		for (var x = (cx - width / doubleScale) - cx % 32; x <= cx + width / doubleScale; x += 32) {
+			line(x, cy - height / doubleScale, x, cy + height / doubleScale);
+		}
+
+		for (var y = (cy - height / doubleScale) - cy % 32; y <= cy + height / doubleScale; y += 32) {
+			line(cx - width / doubleScale, y, cx + width / doubleScale, y);
+		}
+
 		if (room.data) {
 			stroke(0);
 			strokeWeight(4);
 
 			room.data.forEach((d) => {
-				fill(255);
+				var x = d[0]
+				var y = d[1]
+
+				if (Math.abs(cx - x) > width / SCALE * 0.75 || Math.abs(cy - y) > height / SCALE * 0.75) {
+					return
+				}
+
+				var color = d[4]
+
+				if (color) {
+					color = playerColors[color % playerColors.length]
+					fill(color[0], color[1], color[2]);
+				} else {
+					fill(255);
+				}
+
 				textSize(4);
 				textAlign(CENTER, BOTTOM);
-				text(d[3], d[0] - 32, d[1] - 2, 64);
-				fill(200);
-				text(d[2], d[0] - 32, d[1] - 8, 64);
+				text(d[3], x - 32, y - 2, 64);
+
+				if (color) {
+					fill(color[0] * 0.5, color[1] * 0.5, color[2] * 0.5);
+				} else {
+					fill(200);
+				}
+
+				text(d[2], x - 32, y - 8, 64);
 			});
 
 			noStroke();
@@ -146,9 +189,19 @@ function setupDraw() {
 var input = document.getElementById("chat");
 var button = document.getElementById("send");
 
+input.addEventListener("focusin", () => {
+	room.inputBlocked = true;
+});
+
+input.addEventListener("focusout", () => {
+	room.inputBlocked = false;
+});
+
 input.addEventListener("keyup", (event) => {
   if (event.keyCode === 13) {
-    event.preventDefault();
+		event.preventDefault();
+		room.inputBlocked = false;
+		input.blur();
     button.click();
   }
 });
